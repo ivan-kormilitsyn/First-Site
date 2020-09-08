981a1ec227790b160b470453973f0f18a7d0e27f
Ivan <kormilitsynvanya@yandex.ru>
2020-09-08 11:57:09 +0300

    First commit 08/09

---
+++ JS 18.08.js
@@ -0,0 +1,18 @@
+// const AgeQue=prompt("How old are you?");
+// if (AgeQue>=18)
+//     alert("Hello people!")
+// else if
+//     (AgeQue < 18)
+//     alert("your young, sorry")
+
+// const category = 'Toys'
+// console.log(`https://cecsFM.com/${category}/5`);
+//
+//     const name = prompt('Whats you name?')
+//     alert(`Hello, ${name}`)
+
+// console.log(4 + 5);
+// console.log('hello')
+
+
+
